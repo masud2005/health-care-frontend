6acfa7b7d76929337330db7821b43fb31eb95dbf
masud2005 <masud20012005@gmail.com>
2026-05-24 20:23:52 +0600

    steps, testimonials and top rated doctors section added

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,11 +1,17 @@
 import { Hero } from "@/components/modules/home/Hero";
 import Specialities from "@/components/modules/home/Specialties";
+import Steps from "@/components/modules/home/Steps";
+import Testimonials from "@/components/modules/home/Testimonials";
+import TopRatedDoctors from "@/components/modules/home/TopRatedDoctors";
 
 const Home = () => {
     return (
         <div>
             <Hero />
             <Specialities />
+            <TopRatedDoctors />
+            <Steps />
+            <Testimonials />
         </div>
     );
 };
