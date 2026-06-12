5081e29f176cb5ea6d1cb34417a93dca19cfb5dd
masud2005 <masud20012005@gmail.com>
2026-06-12 18:34:02 +0600

    server-fetch, zodvalidator function add and specialities managements all server functionalies implement

---
+++ src/services/admin/specialitiesManagement.ts
@@ -0,0 +1,67 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use server";
+
+import { serverFetch } from "@/lib/server-fetch";
+import { zodValidator } from "@/lib/zodValidator";
+import { createSpecialityZodSchema } from "@/zod/specialities.validation";
+
+export async function createSpeciality(_prevState: any, formData: FormData) {
+  try {
+    const payload = {
+      title: formData.get("title") as string,
+    };
+
+    const validationResult = zodValidator(payload, createSpecialityZodSchema);
+    if (!validationResult.success) {
+      return validationResult;
+    }
+    const validatedPayload = validationResult.data;
+
+    const newFormData = new FormData();
+    newFormData.append("data", JSON.stringify(validatedPayload));
+
+    if (formData.get("file")) {
+      newFormData.append("file", formData.get("file") as Blob);
+    }
+
+    const response = await serverFetch.post("/specialties", {
+      body: newFormData,
+    });
+
+    return response;
+  } catch (error: any) {
+    console.log(error);
+    return {
+      success: false,
+      message: `${process.env.NODE_ENV === "development" ? error.message : "Something went wrong"}`,
+    };
+  }
+}
+
+export async function getSpecialities() {
+    try {
+        const response = await serverFetch.get("/specialties")
+        const result = await response.json();
+        return result;
+    } catch (error: any) {
+        console.log(error);
+        return {
+            success: false,
+            message: `${process.env.NODE_ENV === 'development' ? error.message : 'Something went wrong'}`
+        };
+    }
+}
+
+export async function deleteSpeciality(id: string) {
+    try {
+        const response = await serverFetch.delete(`/specialties/${id}`)
+        const result = await response.json();
+        return result;
+    } catch (error: any) {
+        console.log(error);
+        return {
+            success: false,
+            message: `${process.env.NODE_ENV === 'development' ? error.message : 'Something went wrong'}`
+        };
+    }
+}
